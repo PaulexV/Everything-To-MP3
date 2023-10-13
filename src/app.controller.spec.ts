6dfaaec1c1851f34f7d749ef52b209dcc95c5b77
Paulex <paulalexis.verrier@ynov.com>
2023-10-13 11:07:38 +0200

    chore: add jest types + missing imports

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { beforeEach, expect } from 'bun:test';
 
 describe('AppController', () => {
   let appController: AppController;
