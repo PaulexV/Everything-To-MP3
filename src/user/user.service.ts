4e48eef51fd8f0b7345a398fae6970bdb8b2e28b
Astruum0 <arthurvella33@gmail.com>
2023-11-08 14:39:31 +0100

    feat(userService): add error management if user already exists

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { User, UserDocument } from "./user.schema";
 import { Model } from "mongoose";
 import { randomUUID } from "crypto";
 import { default as bcrypt } from "bcryptjs"
+import { BadRequestError } from "../helper/errorManager"
 
 
 @Injectable()
@@ -16,12 +17,20 @@ export class UserService {
 	private saltRounds = 10
 
 	async findOne(username: string): Promise<User | undefined> {
-		return (await this.userModel.find({username}))[0];
+		return (await this.userModel.find({username}))[0]
 	}
 
-	async create(username: string, password: string): Promise<User> {
+	async alreadyExists(username: string): Promise<boolean> {
+		return (await this.findOne(username)) !== undefined
+	}
+
+	async create(username: string, password: string) {
 		const salt = await bcrypt.genSalt(this.saltRounds)
 		const hash = await bcrypt.hash(password, salt)
+
+		if (this.alreadyExists(username)) {
+			throw BadRequestError("Username already exists")
+		}
 
 		const newUser: User = {
 			id: randomUUID(),
