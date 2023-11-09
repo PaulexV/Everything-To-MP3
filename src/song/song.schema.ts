329ddca9903a90856edabae507701e3debbba12a
Mattéo Christophe <matteo.christophe@opquast.com>
2023-11-09 13:33:02 +0100

    test linting

--- src/song/song.schema.ts
+++ src/song/song.schema.ts
@@ -6,21 +6,21 @@ export type SongDocument = HydratedDocument<Song>
 
 @Schema()
 export class Song {
-  @ApiProperty()
-  @Prop({ required: true })
-  id: string
+    @ApiProperty()
+    @Prop({ required: true })
+    id: string
 
-  @ApiProperty()
-  @Prop({ required: true })
-  title: string
+    @ApiProperty()
+    @Prop({ required: true })
+    title: string
 
-  @ApiProperty()
-  @Prop({ required: true })
-  filename: string
+    @ApiProperty()
+    @Prop({ required: true })
+    filename: string
 
-  @ApiProperty()
-  @Prop({ required: true })
-  originalLink: string
+    @ApiProperty()
+    @Prop({ required: true })
+    originalLink: string
 }
 
 export const SongSchema = SchemaFactory.createForClass(Song)
