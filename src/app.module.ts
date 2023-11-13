896067cd9a5365204d36d6f44e0c35c9c9784a7d
paul-lautier <paul.lautier64@gmail.Com>
2023-11-13 21:18:03 +0100

    detect env and change url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,39 @@
+// import { MongooseModule } from "@nestjs/mongoose"
+// import { JwtService } from "@nestjs/jwt"
+// import { Module } from "@nestjs/common"
+// import { APP_GUARD } from "@nestjs/core"
+// import { AuthGuard } from "./auth/auth.guard"
+// import { SongModule } from "./song/song.module"
+// import { AuthModule } from "./auth/auth.module"
+// import { PlaylistModule } from "./playlist/playlist.module"
+// import { SearchModule } from "./search/search.module"
+// import { UserModule } from "./user/user.module"
+// import * as dotenv from "dotenv"
+
+// dotenv.config()
+
+// @Module({
+//     imports: [
+//         ...(process.env.DOCKER_CONTAINER === "true"
+//             ? [MongooseModule.forRoot(process.env.MONGODB_URL_DOCKER)]
+//             : [MongooseModule.forRoot(process.env.MONGODB_URL_LOCAL)]),
+//         SongModule,
+//         AuthModule,
+//         PlaylistModule,
+//         SearchModule,
+//         UserModule,
+//     ],
+//     controllers: [],
+//     providers: [
+//         {
+//             provide: APP_GUARD,
+//             useClass: AuthGuard,
+//         },
+//         JwtService,
+//     ],
+// })
+// export class AppModule {}
+
 import { MongooseModule } from "@nestjs/mongoose"
 import { JwtService } from "@nestjs/jwt"
 import { Module } from "@nestjs/common"
@@ -8,10 +44,16 @@ import { AuthModule } from "./auth/auth.module"
 import { PlaylistModule } from "./playlist/playlist.module"
 import { SearchModule } from "./search/search.module"
 import { UserModule } from "./user/user.module"
+import { UserService } from "./user/user.service"
+import * as dotenv from "dotenv"
 
+dotenv.config()
 @Module({
     imports: [
-        MongooseModule.forRoot("mongodb://mongodb:27017/e2mp3"),
+        // Conditionnellement importer le module en fonction de l'environnement Docker
+        ...(process.env.DOCKER_CONTAINER === "true"
+            ? [MongooseModule.forRoot(process.env.MONGODB_URL_DOCKER)]
+            : [MongooseModule.forRoot(process.env.MONGODB_URL_LOCAL)]),
         SongModule,
         AuthModule,
         PlaylistModule,
