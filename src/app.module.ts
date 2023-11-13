4df3168756b0beca66f48bc82bf07d855be0b438
paul-lautier <paul.lautier64@gmail.Com>
2023-11-13 21:22:11 +0100

    je te jure ça marche mtn pa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,3 @@
-// import { MongooseModule } from "@nestjs/mongoose"
-// import { JwtService } from "@nestjs/jwt"
-// import { Module } from "@nestjs/common"
-// import { APP_GUARD } from "@nestjs/core"
-// import { AuthGuard } from "./auth/auth.guard"
-// import { SongModule } from "./song/song.module"
-// import { AuthModule } from "./auth/auth.module"
-// import { PlaylistModule } from "./playlist/playlist.module"
-// import { SearchModule } from "./search/search.module"
-// import { UserModule } from "./user/user.module"
-// import * as dotenv from "dotenv"
-
-// dotenv.config()
-
-// @Module({
-//     imports: [
-//         ...(process.env.DOCKER_CONTAINER === "true"
-//             ? [MongooseModule.forRoot(process.env.MONGODB_URL_DOCKER)]
-//             : [MongooseModule.forRoot(process.env.MONGODB_URL_LOCAL)]),
-//         SongModule,
-//         AuthModule,
-//         PlaylistModule,
-//         SearchModule,
-//         UserModule,
-//     ],
-//     controllers: [],
-//     providers: [
-//         {
-//             provide: APP_GUARD,
-//             useClass: AuthGuard,
-//         },
-//         JwtService,
-//     ],
-// })
-// export class AppModule {}
-
 import { MongooseModule } from "@nestjs/mongoose"
 import { JwtService } from "@nestjs/jwt"
 import { Module } from "@nestjs/common"
