807e51790cc3301fb4acb1807b5f6dc3280ae5f4
Paul-Alexis Verrier <57707777+PaulexV@users.noreply.github.com>
2023-11-16 19:01:49 +0100

    Update search.controller.ts

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseInterceptors } from "@nestjs/common"
+import { Controller, Get, Query } from "@nestjs/common"
 import { SearchService } from "./search.service"
 import { Public } from "../auth/auth.service"
 
